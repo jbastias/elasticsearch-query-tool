b9b36dfbd46551231757091c4db47992ccebfa98
jbastias <jorgebastias@hotmail.com>
2018-04-27 00:06:01 -0600

    update the collection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, Input, Button } from 'reactstrap';
+import { Container, TabContent, TabPane, Nav, NavItem, NavLink, Row, Col, Form, Input, Button } from 'reactstrap';
 import classnames from 'classnames';
 import './App.css';
 
 import ResultTable from './components/ResultTable';
-import QueryEditor from './components/QueryEditor';
+// import QueryEditor from './components/QueryEditor';
+
+import AceEditor from 'react-ace';
+
+import { req } from './utils';
+
+import 'brace/ext/language_tools';
+import 'brace/mode/javascript';
+import 'brace/theme/solarized_dark';
+
+const defaultValue =
+`
+{ 
+  "query": { 
+    "constant_score": { 
+      "filter": { 
+        "term": {
+          "objectId": "004h3esPX3"
+        }
+      } 
+    } 
+  } 
+}
+`;
 
 class App extends Component {
 
@@ -12,7 +35,9 @@ class App extends Component {
     super(props);
     this.state = {
       activeTab: '1',
-      value: null,
+      value: defaultValue,
+      results: null,
+      collection: 'presentation'
     };
   }
 
@@ -22,73 +47,49 @@ class App extends Component {
     }
   }
 
-  onChangeContent = (newValue) => {
-    console.log('change: ', newValue);
+  onChange = (newValue) => {
+    // console.log('change: ', newValue);
     this.setState({ value: newValue });
   }
 
-  seeState = () => {
-    alert(this.state.value);
+  updateResults = () => {
+    if (this.state.results) {
+      return JSON.stringify(this.state.results, null, 2);
+    }
+    return '';
+  };
 
-    this.setState({ value: { a: "B" } });
-
+  updateCollection = (e) => {
+    // console.log('val: ', e.target.value);
+    this.setState({ collection: e.target.value });
   };
 
   runQuery = (e) => {
     e.preventDefault();
-    // console.log(this.state.value);
+    const esUrl = process.env.REACT_APP_ES_URL;
+    const index = process.env.REACT_APP_ES_INDEX;
+    const collection = this.state.collection;
+    const searchObj = this.state.value;
 
-    alert(JSON.stringify(this.state.value, null, 2));
-
-
-    // const esUrl = process.env.REACT_APP_ES_URL;
-    // const index = process.env.REACT_APP_ES_INDEX;
-    // const key = process.env.REACT_APP_ES_SECRET_KEY;
-    // const collection = 'presentation';
-
-    // // const { REACT_APP_ES_URL, REACT_APP_ES_INDEX, REACT_APP_ES_SECRET_KEY } = process.env;
-    // // console.log('??', REACT_APP_ES_URL, REACT_APP_ES_INDEX, REACT_APP_ES_SECRET_KEY);
-    // // console.log(esUrl, index, key, collection);
-
-    // const searchObj = this.state.value;
-
-    // const queryObj = { query: { constant_score: { filter: { term: searchObj } } } };
-
-    // return req({
-    //   method: 'GET',
-    //   url: `${esUrl}/${index}/${collection}/_search`,
-    //   body: JSON.stringify(queryObj)
-    //   // ,
-    //   // headers: {
-    //   //   SECRET_KEY: key
-    //   // }
-    // }).then(res => {
-    //   if (res.statusCode !== 200) return null;
-
-    //   // console.log(JSON.stringify(res.body, null, 2));
-
-    //   return JSON.parse(res.body);
-    // }).then(body => {
-
-    //   alert(JSON.stringify(body, null, 2));
-
-    //   if (!body) return null;
-    //   if (body.hits.total === 0) return null;
-
-    //   // console.log(JSON.stringify(body, null, 2));
-
-    //   return body;
-    // });
-
-
-
-
-    // req()
-    // .then(data => console.log(data));
+    return req({
+      method: 'POST',
+      url: `${esUrl}/${index}/${collection}/_search`,
+      body: searchObj,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.statusCode !== 200) return null;
+      return JSON.parse(res.body);
+    }).then(body => {
+      alert(JSON.stringify(body, null, 2));
+      if (!body) return null;
+      if (body.hits.total === 0) return null;
+      this.setState({ results: body });
+      return body;
+    });
 
   }; 
-
-
 
   render() {
     return (
@@ -100,11 +101,44 @@ class App extends Component {
 
           <Row>
             <Col sm="12">
-              <QueryEditor onChange={this.onChangeContent} updateCode={this.updateCode} />
+              {/* <QueryEditor onChange={this.onChangeContent} updateCode={this.updateCode} /> */}
+              <AceEditor
+                mode="javascript"
+                theme="solarized_dark"
+                onChange={this.onChange}
+                value={this.state.value}
+                editorProps={{$blockScrolling: Infinity}}
+                width="100%"
+                height="250px"
+                fontSize={14}
+                showPrintMargin={true}
+                showGutter={true}
+                highlightActiveLine={true}
+                setOptions={{
+                  enableBasicAutocompletion: true,
+                  enableLiveAutocompletion: false,
+                  enableSnippets: true,
+                  showLineNumbers: true,
+                  tabSize: 2,
+                }}
+              />
               <div className="text-left">
-                <Button onClick={this.runQuery} className="mt-1 mb-3" color="success">Run Query</Button>
-                <Button onClick={this.seeState} className="mt-1 mb-3" color="danger">See State</Button>
-                
+                <Form className="form-inline" width="100%" size="lg">
+                  <Button onClick={this.runQuery} className="mt-1 mb-3" color="success">Run Query</Button>
+                  <Input defaultValue="presentation" onChange={this.updateCollection} className="ml-3" type="select" name="select" id="exampleSelect">
+                    <option>cancellation</option>
+                    <option>collaborator</option>
+                    <option>coupon</option>
+                    <option>download-request</option>
+                    <option>formula</option>
+                    <option>palette</option>
+                    <option>presentation-request</option>
+                    <option>presentation</option>
+                    <option>team</option>
+                    <option>user</option>
+                    <option>slide</option>
+                  </Input>
+                </Form>
               </div>
             </Col>
           </Row>
@@ -132,15 +166,13 @@ class App extends Component {
               <TabPane tabId="1">
                 <Row>
                   <Col sm="12">
-                    <h4>Results</h4>
-                    <Input type="textarea" name="text" id="exampleText" />
+                    <Input value={this.updateResults()} rows="25"  type="textarea" name="text" id="exampleText" />
                   </Col>
                 </Row>
               </TabPane>
               <TabPane tabId="2">
                 <Row>
                   <Col sm="12">
-                    <h4>Results Table</h4>
                     <ResultTable />
                   </Col>
                 </Row>
